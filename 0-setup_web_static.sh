a4316a43bb0b0791999bd7b7b534a279793e02de
Samson Wayne <smuhadia4@gmail.com>
2024-04-04 07:40:07 -0700

    corrected script 1

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,28 +1,27 @@
 #!/usr/bin/env bash
-# Write a Bash script that sets up your web servaers for the deployment of web_static. It must:
-# Install Nginx if it not already installed
+# script that sets up web servers for the deployment of web_static
 sudo apt-get update
-sudo apt-get -q -y install nginx
-sudo service nginx start
-# Create the folder /data/ if it doesn't already exist
-# Create the folder /data/web_static/ if it doesn't already exist
-# Create the folder /data/web_static/releases/ if it doesn't already exist
-# Create the folder /data/web_static/shared/ if it doesn't already exist
-# Create the folder /data/web_static/releases/test/ if it doesn't already exist
-sudo mkdir -p /data/web_static/{releases/test,shared}
-# Create a fake HTML file /data/web_static/releases/test/index.html (with simple content, to test your Nginx configuration)
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
+
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
 sudo echo "<html>
-    <head>
-    </head>
-    <body>
-        Holberton School
-    </body>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
-# Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder. If the symbolic link already exists, it should be deleted and recreated every time the script is ran.
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
-# Give ownership of the /data/ folder to the ubuntu user AND group (you can assume this user and group exist). This should be recursive; everything inside should be created/owned by this user/group.
+
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+
 sudo chown -R ubuntu:ubuntu /data/
-# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static (ex: https://mydomainname.tech/hbnb_static). Don't forget to restart Nginx after updating the configuration:
-# Use alias inside your Nginx configuration
-sudo sed -i "39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
 sudo service nginx restart
